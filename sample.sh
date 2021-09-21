b12dac1055328ac089776a506a20a610192e9659
adamabr <adamabertrand@cmail.carleton.ca>
2021-09-21 18:54:52 -0400

    first commit

---
+++ sample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Sample shell script https://www.youtube.com/watch?v=5CnTKT2UEn4
+
+echo "Hello There!" 
+git init
+
+
+read -t 10
+
+#git config --global user.email "adamabertrand@cmail.carleton.ca"
+# git config --global user.name "adamabr"
+# https://github.com/Adamabr/lab2.git
+# git remote add origin https: https://github.com/Adamabr/lab2.git
